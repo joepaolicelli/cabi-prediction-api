b1eceeb6f786e1d90ce024ce71772909445db598
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-07-28 15:42:26 -0400

    Consolidate duplicate code in cabi.prepare_data

--- cabi/prepare_data/temporal_only.py
+++ cabi/prepare_data/temporal_only.py
@@ -1,10 +1,8 @@
 # temporal_only
 # The only features are the current time, day, and month.
 
-from cabi.prepare_data.utils import bal
-import datetime
+from cabi.prepare_data.utils import bal, get_and_adjust_data
 import numpy as np
-import pandas as pd
 
 
 def temporal_only(
@@ -13,32 +11,9 @@ def temporal_only(
     """
     sample_size will be ignored if balance is not None.
     """
-    df_orig = pd.read_sql_query(
-        "SELECT * FROM outages "
-        + "WHERE %s;" % selector, db_engine)
 
-    # "5T" is 5 minutes.
-    dti = pd.date_range(0, -1, freq="5T")
-    data = pd.Series("", index=dti)
-
-    # Merge each outage into dataframe.
-    for outage in df_orig.itertuples():
-        ostart = pd.to_datetime(outage[4], infer_datetime_format=True)
-        + datetime.timedelta(seconds=150)
-        ostart = ostart.replace(
-            minute=(ostart.minute - (ostart.minute % 5)), second=0)
-        oend = pd.to_datetime(outage[5], infer_datetime_format=True)
-        + datetime.timedelta(seconds=150)
-        oend = oend.replace(
-            minute=(oend.minute - (oend.minute % 5)), second=0)
-
-        index = pd.date_range(ostart, oend, freq="5T")
-
-        data = pd.concat([data, pd.Series(outage[3], index=index)])
-
-    data.sort_index(inplace=True)
-    data = data.groupby(data.index).first()
-    data = data.reindex(pd.date_range(start, end, freq="5T"))
+    data = get_and_adjust_data(
+        db_engine, selector, start, end, sample_size)
 
     # Balance or set to sample_size
     if balance is None:
--- cabi/prepare_data/utils.py
+++ cabi/prepare_data/utils.py
@@ -1,4 +1,36 @@
+import datetime
 import pandas as pd
+
+
+def get_and_adjust_data(db_engine, selector, start, end, sample_size):
+    df_orig = pd.read_sql_query(
+        "SELECT * FROM outages "
+        + "WHERE %s;" % selector, db_engine)
+
+    # "5T" is 5 minutes.
+    dti = pd.date_range(0, -1, freq="5T")
+    data = pd.Series("", index=dti)
+
+    # Merge each outage into dataframe.
+    for outage in df_orig.itertuples():
+        ostart = pd.to_datetime(outage[4], infer_datetime_format=True)
+        + datetime.timedelta(seconds=150)
+        ostart = ostart.replace(
+            minute=(ostart.minute - (ostart.minute % 5)), second=0)
+        oend = pd.to_datetime(outage[5], infer_datetime_format=True)
+        + datetime.timedelta(seconds=150)
+        oend = oend.replace(
+            minute=(oend.minute - (oend.minute % 5)), second=0)
+
+        index = pd.date_range(ostart, oend, freq="5T")
+
+        data = pd.concat([data, pd.Series(outage[3], index=index)])
+
+    data.sort_index(inplace=True)
+    data = data.groupby(data.index).first()
+    data = data.reindex(pd.date_range(start, end, freq="5T"))
+
+    return data
 
 
 def bal(df, balance):
