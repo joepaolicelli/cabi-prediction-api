faa34ca2d718b0bd3213f1b6a56a51999093e9d7
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-01 16:37:50 -0400

    Add script to store current Capital Bikeshare station statuses in database

---
+++ tools/cabi_recorder.py
@@ -0,0 +1,46 @@
+"""
+Records the status of all capital bikeshare stations.
+
+A postgres database is required. The environmental variable 'CABI_DB' should
+be set to a connection string of the format
+'username:password@host:port/db_name'.
+
+Time is stored in UTC.
+"""
+
+import os
+import pandas as pd
+import requests
+from sqlalchemy import create_engine, sql
+from sqlalchemy import Column, MetaData, Table
+from sqlalchemy.types import DateTime, Integer
+import xmltodict
+
+# Establish database connection.
+engine = create_engine("postgresql+psycopg2://" + os.environ["CABI_DB"])
+conn = engine.connect()
+
+if not engine.has_table("bike_count"):
+    meta = MetaData()
+    counts_table = Table(
+        "bike_count", meta,
+        Column("station_id", Integer),
+        Column("ts", DateTime),
+        Column("bikes", Integer),
+        Column("spaces", Integer))
+    meta.create_all(engine)
+
+data = xmltodict.parse(requests.get(
+    "https://feeds.capitalbikeshare.com/stations/stations.xml").text)
+
+for st in data["stations"]["station"]:
+    ts = pd.to_datetime(
+        st["lastCommWithServer"], unit="ms", infer_datetime_format=True)
+
+    query = sql.text(
+        "INSERT INTO bike_count (station_id, ts, bikes, spaces) "
+        "VALUES (:station_id, :ts, :bikes, :spaces)")
+
+    conn.execute(
+        query, station_id=st["terminalName"], ts=ts,
+        bikes=st["nbBikes"], spaces=st["nbEmptyDocks"])
