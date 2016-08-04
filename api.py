42cc76b93d136589aba6f5449bab0cafc73d771f
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-04 11:37:59 -0400

    Add API for predictions

---
+++ api.py
@@ -0,0 +1,29 @@
+from cabi.predict import predict
+import falcon
+import json
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+class StationStatus(object):
+    def on_post(self, req, resp):
+        query = json.loads(req.stream.read().decode('utf-8'))
+
+        engine = create_engine(
+            "postgresql+psycopg2://" + os.environ["CABI_DB"])
+
+        ts = pd.to_datetime(query["time"], infer_datetime_format=True)
+
+        pred = predict(
+            "temp/model", engine, int(query["station_id"]), ts)
+
+        resp.body = json.dumps({
+            "prediction": {
+                "empty": pred["empty"],
+                "full": pred["full"]}})
+
+station_status = StationStatus()
+
+app = falcon.API()
+app.add_route('/', station_status)
