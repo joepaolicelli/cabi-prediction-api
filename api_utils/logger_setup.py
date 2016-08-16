3dbe7ee1428cd75eca5cd3790bd9cefd7e7c76aa
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-16 12:04:12 -0400

    Add logging

---
+++ api_utils/logger_setup.py
@@ -0,0 +1,17 @@
+import logging
+
+
+def logger_setup():
+    logger = logging.getLogger("cabi_api")
+    fh = logging.FileHandler('cabi_api.log')
+    fh.setLevel(logging.DEBUG)
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.ERROR)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(sh)
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,30 +8,40 @@ if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
 from api_utils.auth import authorized       # noqa: E402 (Imports not at top)
-from cabi.predict import predict            # noqa: E402
-import falcon                               # noqa: E402
-import json                                 # noqa: E402
-import pandas as pd                         # noqa: E402
-from sqlalchemy import create_engine        # noqa: E402
+from api_utils.logger_setup import logger_setup     # noqa: E402
+from cabi.predict import predict                    # noqa: E402
+import falcon                                       # noqa: E402
+import json                                         # noqa: E402
+import logging                                      # noqa: E402
+import pandas as pd                                 # noqa: E402
+from sqlalchemy import create_engine                # noqa: E402
+
+logger_setup()
+logger = logging.getLogger("cabi_api")
 
 
 class StationStatus(object):
     @falcon.before(authorized)
     def on_post(self, req, resp):
-        query = json.loads(req.stream.read().decode('utf-8'))
+        logger.info("POST request received.")
+        try:
+            query = json.loads(req.stream.read().decode('utf-8'))
 
-        engine = create_engine(
-            "postgresql+psycopg2://" + os.environ["CABI_DB"])
+            engine = create_engine(
+                "postgresql+psycopg2://" + os.environ["CABI_DB"])
 
-        ts = pd.to_datetime(query["time"], infer_datetime_format=True)
+            ts = pd.to_datetime(query["time"], infer_datetime_format=True)
 
-        pred = predict(
-            "temp/model", engine, int(query["station_id"]), ts)
+            pred = predict(
+                "temp/model", engine, int(query["station_id"]), ts)
 
-        resp.body = json.dumps({
-            "prediction": {
-                "empty": pred["empty"],
-                "full": pred["full"]}})
+            resp.body = json.dumps({
+                "prediction": {
+                    "empty": pred["empty"],
+                    "full": pred["full"]}})
+
+        except Exception as err:
+            logger.error(err)
 
 station_status = StationStatus()
 
