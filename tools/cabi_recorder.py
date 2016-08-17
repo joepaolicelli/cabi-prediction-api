eef30a83e05b6eeb29719f223407805dcb6e2876
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-17 11:51:53 -0400

    Data recording scripts print errors

--- tools/cabi_recorder.py
+++ tools/cabi_recorder.py
@@ -17,33 +17,38 @@ from sqlalchemy import Column, MetaData, Table
 from sqlalchemy.types import DateTime, Integer
 import xmltodict
 
-# Establish database connection.
-engine = create_engine("postgresql+psycopg2://" + os.environ["CABI_DB"])
-conn = engine.connect()
+try:
+    # Establish database connection.
+    engine = create_engine("postgresql+psycopg2://" + os.environ["CABI_DB"])
+    conn = engine.connect()
 
-if not engine.has_table("bike_count"):
-    meta = MetaData()
-    counts_table = Table(
-        "bike_count", meta,
-        Column("station_id", Integer),
-        Column("ts", DateTime),
-        Column("bikes", Integer),
-        Column("spaces", Integer))
-    meta.create_all(engine)
+    if not engine.has_table("bike_count"):
+        meta = MetaData()
+        counts_table = Table(
+            "bike_count", meta,
+            Column("station_id", Integer),
+            Column("ts", DateTime),
+            Column("bikes", Integer),
+            Column("spaces", Integer))
+        meta.create_all(engine)
 
-data = xmltodict.parse(requests.get(
-    "https://feeds.capitalbikeshare.com/stations/stations.xml").text)
+    data = xmltodict.parse(requests.get(
+        "https://feeds.capitalbikeshare.com/stations/stations.xml").text)
 
-for st in data["stations"]["station"]:
-    ts = pd.to_datetime(
-        st["lastCommWithServer"], unit="ms", infer_datetime_format=True)
-    # Convert from UTC to Eastern time.
-    ts = ts.replace(tzinfo=pytz.utc).astimezone(pytz.timezone("US/Eastern"))
+    for st in data["stations"]["station"]:
+        ts = pd.to_datetime(
+            st["lastCommWithServer"], unit="ms", infer_datetime_format=True)
+        # Convert from UTC to Eastern time.
+        ts = ts.replace(tzinfo=pytz.utc).astimezone(
+            pytz.timezone("US/Eastern"))
 
-    query = sql.text(
-        "INSERT INTO bike_count (station_id, ts, bikes, spaces) "
-        "VALUES (:station_id, :ts, :bikes, :spaces)")
+        query = sql.text(
+            "INSERT INTO bike_count (station_id, ts, bikes, spaces) "
+            "VALUES (:station_id, :ts, :bikes, :spaces)")
 
-    conn.execute(
-        query, station_id=st["terminalName"], ts=ts,
-        bikes=st["nbBikes"], spaces=st["nbEmptyDocks"])
+        conn.execute(
+            query, station_id=st["terminalName"], ts=ts,
+            bikes=st["nbBikes"], spaces=st["nbEmptyDocks"])
+
+except Exception as err:
+    print(err)
--- tools/weather_recorder.py
+++ tools/weather_recorder.py
@@ -13,32 +13,36 @@ from sqlalchemy import create_engine, sql
 from sqlalchemy import Column, MetaData, Table
 from sqlalchemy.types import DateTime, Float
 
-# Establish database connection.
-engine = create_engine("postgresql+psycopg2://" + os.environ["CABI_DB"])
-conn = engine.connect()
+try:
+    # Establish database connection.
+    engine = create_engine("postgresql+psycopg2://" + os.environ["CABI_DB"])
+    conn = engine.connect()
 
-if not engine.has_table("weather"):
-    meta = MetaData()
-    weather_table = Table(
-        "weather", meta,
-        Column("ts", DateTime),
-        Column("temp", Float),
-        Column("precip", Float))
-    meta.create_all(engine)
+    if not engine.has_table("weather"):
+        meta = MetaData()
+        weather_table = Table(
+            "weather", meta,
+            Column("ts", DateTime),
+            Column("temp", Float),
+            Column("precip", Float))
+        meta.create_all(engine)
 
-req = requests.get(
-    "http://api.wunderground.com/api/%s/conditions/q/DC/Washington.json"
-    % os.environ["WUNDERGROUND_KEY"]).json()
+    req = requests.get(
+        "http://api.wunderground.com/api/%s/conditions/q/DC/Washington.json"
+        % os.environ["WUNDERGROUND_KEY"]).json()
 
-ts = pd.to_datetime(
-    req["current_observation"]["local_time_rfc822"],
-    infer_datetime_format=True)
+    ts = pd.to_datetime(
+        req["current_observation"]["local_time_rfc822"],
+        infer_datetime_format=True)
 
-query = sql.text(
-    "INSERT INTO weather (ts, temp, precip) "
-    "VALUES (:ts, :temp, :precip)")
+    query = sql.text(
+        "INSERT INTO weather (ts, temp, precip) "
+        "VALUES (:ts, :temp, :precip)")
 
-conn.execute(
-    query, ts=ts,
-    temp=float(req["current_observation"]["temp_c"]),
-    precip=float(req["current_observation"]["precip_1hr_metric"]))
+    conn.execute(
+        query, ts=ts,
+        temp=float(req["current_observation"]["temp_c"]),
+        precip=float(req["current_observation"]["precip_1hr_metric"]))
+
+except Exception as err:
+    print(err)
