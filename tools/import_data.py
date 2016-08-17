c61ce270b7e6f5b1e27174b7f1eb5ca41b687d3b
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-17 12:08:58 -0400

    Environmental variable can be used for db info with import tools

--- tools/import_data.py
+++ tools/import_data.py
@@ -1,31 +1,35 @@
-# Script to import historical station status data into a postgres database.
+# Script to import historical station outage data into a postgres database,
+# in the format available for export from the excellent CaBi Tracker:
+# http://cabitracker.com/outage_history.php
 #
 # Run this file directly, with all files to import listed as command line
 # arguments. To clear the database table, make the first argument "clear".
 #
-# Database information must be entered below.
+# Database information must be entered below or in the env var CABI_DB.
 #
 # Also, if "import_data.py" is in the name of the file you're trying to
 # import, it's not going to work. This is to prevent this code from trying
 # to insert itself into the database.
 
+import os
 import pandas as pd
 import sqlalchemy
 from sqlalchemy import create_engine
 import sys
 
-# Database information.
+# Database information, if not contained in the env var CABI_DB.
 host = ""
 port = ""
 db_name = ""
 user = ""
 password = ""
 
+db_str = os.environ["CABI_DB"]
+if db_str is None:
+    db_str = user + ":" + password + "@" + host + ":" + port + "/" + db_name
+
 # Initialize database connection.
-engine = create_engine(
-    "postgresql+psycopg2://"
-    + user + ":" + password + "@"
-    + host + ":" + port + "/" + db_name)
+engine = create_engine("postgresql+psycopg2://" + db_str)
 
 if sys.argv[1] == "clear" and engine.has_table("outage"):
     engine.execute("DROP TABLE outage;")
--- tools/import_weather_data.py
+++ tools/import_weather_data.py
@@ -5,30 +5,32 @@
 # Run this file directly, with all files to import listed as command line
 # arguments. To clear the database table, make the first argument "clear".
 #
-# Database information must be entered below.
+# Database information must be entered below or in the env var CABI_DB.
 #
 # Also, if "import_weather_data.py" is in the name of the file you're trying
 # to import, it's not going to work. This is to prevent this code from trying
 # to insert itself into the database.
 
+import os
 import pandas as pd
 import pytz
 import sqlalchemy
 from sqlalchemy import create_engine
 import sys
 
-# These must be added for this to work.
+# Database information, if not contained in the env var CABI_DB.
 host = ""
 port = ""
 db_name = ""
 user = ""
 password = ""
 
+db_str = os.environ["CABI_DB"]
+if db_str is None:
+    db_str = user + ":" + password + "@" + host + ":" + port + "/" + db_name
+
 # Initialize database connection.
-engine = create_engine(
-    "postgresql+psycopg2://"
-    + user + ":" + password + "@"
-    + host + ":" + port + "/" + db_name)
+engine = create_engine("postgresql+psycopg2://" + db_str)
 
 if sys.argv[1] == "clear" and engine.has_table("weather_isd"):
     engine.execute("DROP TABLE weather_isd;")
