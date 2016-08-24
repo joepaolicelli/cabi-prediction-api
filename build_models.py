7e8127c60de6f78dde5201532f80562023df2e6e
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-24 09:45:50 -0400

    Don't crash if some models cannot be built

--- build_models.py
+++ build_models.py
@@ -24,15 +24,21 @@ try:
 
     # Build and save a model for each station.
     for row in id_list:
-        station_id = row[0]
-        model_path = "models/station_" + str(station_id)
+        try:
+            station_id = row[0]
+            model_path = "models/station_" + str(station_id)
 
-        # Create folder for model if needed.
-        if not os.path.isdir(model_path):
-            os.makedirs(model_path)
-        print("Station", str(station_id))
-        create_model(
-            model_path + "/model", engine, station_id, START_DATE, END_DATE)
+            # Create folder for model if needed.
+            if not os.path.isdir(model_path):
+                os.makedirs(model_path)
+            print("Station", str(station_id))
+            create_model(
+                model_path + "/model", engine, station_id,
+                START_DATE, END_DATE)
+
+        except Exception as err:
+            print("Error while building model for station", str(station_id))
+            print(traceback.format_exc())
 
 except Exception as err:
     print(datetime.datetime.now().strftime('%c'))
