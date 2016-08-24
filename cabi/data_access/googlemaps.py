c8188d93a7bb4058565fdb6866f5700d61357623
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-24 13:02:48 -0400

    Add bound to geocoding

--- cabi/data_access/googlemaps.py
+++ cabi/data_access/googlemaps.py
@@ -5,6 +5,11 @@ import os
 def get_loc_info(location):
     gmaps = googlemaps.Client(key=os.environ["GMAPS_KEY"])
 
-    results = gmaps.geocode(location)
+    # The bounds, which roughly surround DC and the nearby areas with Capital
+    # Bikeshare, give preference to returning results from this area. This
+    # allows leaving the city off of the entered address.
+    results = gmaps.geocode(location, bounds={
+        "northeast": (39.294891, -76.757937),
+        "southwest": (38.698693, -77.299981)})
 
     return results[0]
