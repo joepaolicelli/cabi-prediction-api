759e94d8f0e5521c4502c84a3dd2e0052e627bc0
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-24 17:45:19 -0400

    Timezone fix for forecasts

--- tools/get_forecast.py
+++ tools/get_forecast.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import pytz
 import requests
 from sqlalchemy import create_engine, sql
 from sqlalchemy import Column, MetaData, Table
@@ -32,9 +33,14 @@ try:
         "VALUES (:ts, :temp, :precip, :condition)")
 
     for hour in req["hourly_forecast"]:
+        ts = datetime.datetime.fromtimestamp(int(hour["FCTTIME"]["epoch"]))
+
+        ts = ts.replace(tzinfo=pytz.utc).astimezone(
+            pytz.timezone("US/Eastern")).replace(tzinfo=None)
+
         conn.execute(
             query,
-            ts=datetime.datetime.fromtimestamp(int(hour["FCTTIME"]["epoch"])),
+            ts=ts,
             temp=float(hour["temp"]["metric"]),
             precip=float(hour["qpf"]["english"]),
             condition=hour["condition"])
