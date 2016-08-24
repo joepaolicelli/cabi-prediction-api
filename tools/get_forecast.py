6428326a7722c5d8d446759f1203ff4ca3b66245
Joe Paolicelli <jpaolicelli@missiondata.com>
2016-08-24 18:47:25 -0400

    Final, actual fix to forecast timezone issue

--- tools/get_forecast.py
+++ tools/get_forecast.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 import pandas as pd
-import pytz
 import requests
 from sqlalchemy import create_engine, sql
 from sqlalchemy import Column, MetaData, Table
@@ -35,9 +34,6 @@ try:
 
     for hour in req["hourly_forecast"]:
         ts = pd.to_datetime(hour["FCTTIME"]["pretty"])
-        # Convert from UTC to Eastern time.
-        ts = ts.replace(tzinfo=pytz.utc).astimezone(
-            pytz.timezone("US/Eastern")).replace(tzinfo=None)
 
         conn.execute(
             query,
